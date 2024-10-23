b4a4b4edbf06ddad47b8e6d9736db7ad8e7e21a1
James <Ja_ari2145@hotmail.com>
2024-10-22 23:01:13 -0500

    cambio

--- login/app.js
+++ login/app.js
@@ -73,7 +73,7 @@ app.post('/auth', async (req, res)=>{
                 res.render('login',{
                     alert: true,
                     alertTitle: 'Error',
-                    alertMessage: '¡Usuario y/o password incorrecto!',
+                    alertMessage: '¡Usuario y/o password incorrecto',
                     alertIcon: 'error',
                     showConfirmButton: true,
                     timer: false,
